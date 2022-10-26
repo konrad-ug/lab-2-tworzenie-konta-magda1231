1920d0cb8aa98724a4b9e97eba3208b58fae67ec
Magda <111302737+magda1231@users.noreply.github.com>
2022-10-26 21:44:22 +0200

    feature 2-4

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,16 @@
+import re
 class Konto:
-    def __init__(self,imie,nazwisko):
+    def __init__(self,imie,nazwisko,pesel,kod = None):
         self.imie = imie
         self.nazwisko = nazwisko 
-        self.saldo = 0 
+        self.saldo = 0
+        self.pesel =  pesel if (len(pesel) == 11) else  "Niepoprawny pesel"
+        if (kod != None and re.fullmatch(r"PROM_.{3}",kod)):
+            self.saldo = self.saldo + 50
+
+
+   
+
+
+
+
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -4,10 +4,43 @@ from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
 
-    def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+    def test_tworzenie_konta(self): 
+        pesel = "11111111111"
+        saldo = "50zł"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", pesel)
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, pesel , "Niepoprawny pesel")
 
-    #tutaj proszę dodawać nowe testy
+
+    def test_zbyt_dlugi_pesel(self): 
+        pesel = "1111111111111"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", pesel)
+        self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel", "Zła wartość wyświetlana w przypadku zbyt długiego peselu")
+
+    def test_zbyt_krotki_pesel(self): 
+        pesel = "11111"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", pesel)
+        self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel", "Zła wartość wyświetlana w przypadku zbyt krótkiego peselu")
+
+    def test_zbyt_długi_kod(self):
+        kod = "PROM_1231223"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "1111111111111", kod)
+        self.assertEqual(pierwsze_konto.saldo, 0, "Zbyt długi kod zaakceptowany" )
+
+    def test_zbyt_krótki_kod(self): 
+        kod = "PROM_12"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "1111111111111", kod)
+        self.assertEqual(pierwsze_konto.saldo, 0, "Zbyt długi kod zaakceptowany" )
+
+    def test_zły_kod(self):
+        kod = "KOD_PROMOCYJNY"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "1111111111111", kod)
+        self.assertEqual(pierwsze_konto.saldo, 0, "Zły kod zaakceptowany" )
+
+    def test_saldo_z_dobrym_kodem(self):
+        kod = "PROM_3.1"
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "1111111111111", kod)
+        self.assertEqual(pierwsze_konto.saldo, 50, "Zła ilość pieniędzy" )
+
