256b9af28431aa55b66d8c32381cdeaaf39164fa
Magda <111302737+magda1231@users.noreply.github.com>
2022-11-23 16:46:16 +0100

    czesc1 feature12

--- app/Konto.py
+++ app/Konto.py
@@ -46,12 +46,101 @@ class Konto:
             self.saldo = self.saldo - kwota - self.oplata 
             self.historia.append(-kwota)
             self.historia.append(-self.oplata)
+
+    # def zaciagnij_kredyt(self,kwota):
+    #     last3 = self.historia[-3:] 
+    #     if(len(last3) == 3):
+    #         for i in last3:
+    #                 if(i<0):
+    #                     print(i)
+    #                     return False
+    #         self.saldo = self.saldo + kwota
+    #         return True
+    #     else:
+    #         return False
+
+
+    def zaciagnij_kredyt(self,kwota):
+        last5 = self.historia[-5:]
+        if(len(last5) == 3):
+            for i in last5:
+                    if(i<0):         
+                        return False
+            self.saldo=self.saldo+ kwota
+            return True
+        if(len(last5) == 5): 
+            suma = 0
+            for i in last5:
+                suma = suma + i 
+            if(suma > kwota):
+                self.saldo = self.saldo + kwota
+                return True
+            else:
+                return False 
+        else:
+            return False 
+
+# konto = Konto("Ala","Kowalska","1111111111")
+# konto.zaciagnij_kredyt(500)
+# historia = [1000,-500,500,200,-200]
+# historia = [1000,-500,500,200,-200]
+# def zaciagnij_kredyt(kwota):
+#         saldo = 1000
+#         last3 = historia[-3:]
+#         last5 = historia[-5:] 
+#         if(len(last3) == 3 and len(last5) != 5):
+#             for i in last3:
+#                     if(i<0):
+#                         print(i)
+#                         return False
+#             saldo = saldo + kwota
+#             return True
+#         elif(len(last5)== 5):
+#             suma = 0
+#             for i in last5:
+#                 suma += i
+#             if(suma>0):
+#                 return True
+#             else:
+#                 return False 
+#         else:
+#             return False 
             
+# print(zaciagnij_kredyt(500))
 
-
-
-
+# last5 = historia[-5:]
+# suma = 0
+# for i in last5:
+#     suma += i
+# print(suma)
+# historia = [1000,300,400]
+# saldo = 1700
+# def zaciagnij_kredyt(kwota):
         
+
+#         last5 = historia[-5:]
+#         if(len(last5) == 3):
+#             for i in last5:
+#                     if(i<0):         
+#                         return False
+#             saldo = saldo+kwota
+#             print(saldo)
+#             return True
+#         if(len(last5) == 5): 
+#             suma = 0
+#             for i in last5:
+#                 suma = suma + i 
+#             if(suma > kwota):
+#                 saldo = saldo + kwota
+#                 return True
+#             else:
+#                 return False 
+#         else:
+#             return False 
+
+# zaciagnij_kredyt(500)
+        
+
 
 
    
@@ -71,3 +160,5 @@ class Konto:
 
 
 
+historia = [1000,-500,500,200,-200]
+print(sum(historia))
---
+++ app/tests/test_kredyt.py
@@ -0,0 +1,102 @@
+import unittest
+import parameterized
+
+
+from ..Konto import Konto
+
+class TestKredut(unittest.TestCase):
+    imie = "Darek"
+    nazwisko = "Kowalski"
+    pesel = "1111111111"
+
+    def setUp(self):
+        self.konto = Konto(self.imie,self.nazwisko,self.pesel)
+
+    def test_zaciagnij_kredyt_udany_1(self):
+        self.konto.historia = [1000,300,400]
+        self.konto.saldo = sum(self.konto.historia)
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo + 500
+        self.assertTrue(self.konto.zaciagnij_kredyt(500))
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_nieudany_pusta_konto_1(self):
+        self.konto.historia = []
+        self.konto.saldo = sum(self.konto.historia)
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    
+    def test_zaciagnij_kredyt_niudany_1_puste_konto(self):
+        self.konto.historia = []
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo 
+        self.assertFalse(self.konto.zaciagnij_kredyt(500))
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_nieudany_zbyt_mało_trans_plus_1(self):
+        self.konto.historia = [-500,100,200]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_nieudany_zbyt_mało_trans_1(self):
+        self.konto.historia = [200,300]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+
+    def test_zaciagnij_kredyt_nieudany_1(self):
+        self.konto.historia = [200,-500,100,500,200,-200]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_nieudany_1_1(self):
+        self.konto.historia = [100,-1]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo 
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_udany_2(self):
+        self.konto.historia = [1000,-500,500,200,-200]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo + 500
+        self.assertEqual(self.konto.zaciagnij_kredyt(500),True,'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+    
+    def test_zaciagnij_kredyt_udany_2_6_w_historii(self):
+        self.konto.historia = [-1000,-500,500,200,-200,600]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo +500
+        self.assertTrue(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_nieudany_2(self):
+        self.konto.historia = [-500,100,-500,300,200]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+    def test_zaciagnij_kredyt_nieudany_2_rowno(self):
+        self.konto.historia = [-100,100,-400,200,200]
+        self.konto.saldo = sum(self.konto.historia)
+        x = self.konto.saldo 
+        self.assertFalse(self.konto.zaciagnij_kredyt(500),'Zła zwrócona wartość')
+        self.assertEqual(self.konto.saldo,x,'Złe saldo konta')
+
+
+
+   
+    
+        
+        
+
