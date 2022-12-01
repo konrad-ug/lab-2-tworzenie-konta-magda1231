3c91088c5632d268eb7c646550d8e3274c61c020
Magda <111302737+magda1231@users.noreply.github.com>
2022-12-01 15:09:54 +0100

    ala

--- app/tests/test_rejestr.py
+++ app/tests/test_rejestr.py
@@ -12,40 +12,36 @@ class TestRejestrKont(unittest.TestCase):
     pesel2 = "11111111111"
 
     @classmethod
-    def SetUpClass(cls):
+    def setUpClass(cls):
         cls.konto = Konto(cls.imie,cls.nazwisko,cls.pesel)
         RejestrKont.dodaj_konto(cls.konto)
-
 
     @classmethod
     def tearDownClass(cls):
         RejestrKont.lista = []
 
-    @classmethod
-    def test_1_dodaj_konto(cls):
-        cls.assertEqual(RejestrKont.ile_kont(),0)
 
    
-    def test_1_dodaj_konto(self):
+    def test_2_dodaj_konto(self):
         konto = Konto(self.imie,self.nazwisko,self.pesel)
         RejestrKont.dodaj_konto(konto)
-        self.assertEqual(RejestrKont.ile_kont(),1)
+        self.assertEqual(RejestrKont.ile_kont(),2)
 
-    def test_2_dodaj_2_konta(self):
+    def test_3_dodaj_2_konta(self):
         konto2 = Konto(self.imie,self.nazwisko,self.pesel)
         RejestrKont.dodaj_konto(konto2)
-        self.assertEqual(RejestrKont.ile_kont(),2)
+        self.assertEqual(RejestrKont.ile_kont(),3)
 
-    def test_3_ile_kont(self):
+    def test_4_ile_kont(self):
         RejestrKont.ile_kont()
-        self.assertEqual(RejestrKont.ile_kont(),2)
+        self.assertEqual(RejestrKont.ile_kont(),3)
 
-    def test_4_znajdz_po_peselu(self):
+    def test_5_znajdz_po_peselu(self):
         konto = Konto(self.imie,self.nazwisko,"21111111111")
         RejestrKont.dodaj_konto(konto)
         self.assertEqual(RejestrKont.znajdz_konto("21111111111"),konto)
 
-    def test_5_znajdz_po_peselu_zły_pesel(self):
+    def test_6_znajdz_po_peselu_zły_pesel(self):
         self.assertEqual(RejestrKont.znajdz_konto("111111111"),"To jest zły pesel")
 
 
