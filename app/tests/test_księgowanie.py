8e486abe7aa3505b3aedd78e3f00176123526639
Magda <111302737+magda1231@users.noreply.github.com>
2022-12-26 20:45:44 +0100

    Revert "Revert "15""

--- app/tests/test_księgowanie.py
+++ app/tests/test_księgowanie.py
@@ -1,64 +1,64 @@
 import unittest
 
-from ..Konto import Konto
+from ..Account import Account
 
-class TestKsięgowanieKonto(unittest.TestCase):
-    imie = "Darek"
-    nazwisko = "Kowalski"
+class TestKsięgowanieAccount(unittest.TestCase):
+    name = "Darek"
+    surname = "Kowalski"
     pesel = "1111111111"
 
     def test_nieudany_przelew_wychodzący(self):
-       konto = Konto(self.imie,self.nazwisko,self.pesel)
-       konto.saldo = 100
-       konto.zaksięguj_przelew_wychodzący(800)
-       self.assertEqual(konto.saldo, 100)
+       account = Account(self.name,self.surname,self.pesel)
+       account.balance = 100
+       account.zaksięguj_przelew_wychodzący(800)
+       self.assertEqual(account.balance, 100)
 
     def test_ujemny_przelew_wychodzący(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 1000
-        konto.zaksięguj_przelew_wychodzący(-800)
-        self.assertEqual(konto.saldo, 1000)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 1000
+        account.zaksięguj_przelew_wychodzący(-800)
+        self.assertEqual(account.balance, 1000)
     
     def test_udany_przelew_wychodzący(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 1000
-        konto.zaksięguj_przelew_wychodzący(800)
-        self.assertEqual(konto.saldo, 1000-800)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 1000
+        account.zaksięguj_przelew_wychodzący(800)
+        self.assertEqual(account.balance, 1000-800)
 
     def test_udany_przelew_przychodący(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 1000
-        konto.zaksięguj_przelew_przychodzący(800)
-        self.assertEqual(konto.saldo, 1000+800)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 1000
+        account.zaksięguj_przelew_przychodzący(800)
+        self.assertEqual(account.balance, 1000+800)
 
     def test_ujemny_przelew_przychodzący(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 1000
-        konto.zaksięguj_przelew_przychodzący(-800)
-        self.assertEqual(konto.saldo, 1000)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 1000
+        account.zaksięguj_przelew_przychodzący(-800)
+        self.assertEqual(account.balance, 1000)
 
 
     def test_udany_wychodzący_przelew_ekspresowy(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 1000
-        konto.zaksięguj_wychodzacy_przelew_ekspresowy(800)
-        self.assertEqual(konto.saldo, 1000-800-1)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 1000
+        account.zaksięguj_wychodzacy_przelew_ekspresowy(800)
+        self.assertEqual(account.balance, 1000-800-1)
 
-    def test_udany_wychodzący_przelew_ekspresowy_konto_ujemne_o_opłatę(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 800
-        konto.zaksięguj_wychodzacy_przelew_ekspresowy(800)
-        self.assertEqual(konto.saldo, 800-800-1)
+    def test_udany_wychodzący_przelew_ekspresowy_account_ujemne_o_opłatę(self):
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 800
+        account.zaksięguj_wychodzacy_przelew_ekspresowy(800)
+        self.assertEqual(account.balance, 800-800-1)
 
     def test_nieudany_wychodzący_przelew_ekspresowy(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 700
-        konto.zaksięguj_wychodzacy_przelew_ekspresowy(800)
-        self.assertEqual(konto.saldo,700)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 700
+        account.zaksięguj_wychodzacy_przelew_ekspresowy(800)
+        self.assertEqual(account.balance,700)
 
     def test_ujemny_wychodzący_przelew_ekspresowy(self):
-        konto = Konto(self.imie,self.nazwisko,self.pesel)
-        konto.saldo = 700
-        konto.zaksięguj_wychodzacy_przelew_ekspresowy(-800)
-        self.assertEqual(konto.saldo,700)
+        account = Account(self.name,self.surname,self.pesel)
+        account.balance = 700
+        account.zaksięguj_wychodzacy_przelew_ekspresowy(-800)
+        self.assertEqual(account.balance,700)
 
--- app/tests/test_księgowanie_konto_firmowe.py
+++ app/tests/test_księgowanie_konto_firmowe.py
@@ -1,40 +1,40 @@
 import unittest
 
-from ..Konto_firmowe import KontoFirmowe
+from ..Company_Account import CompanyAccount
 
-class TestKsięgowanieKontoFirmowe(unittest.TestCase):
-    nazwa_firmy = "company sp.zoo"
+class TestKsięgowanieCompanyAccount(unittest.TestCase):
+    company_name = "company sp.zoo"
     nip = "1111111111"
 
     def test_działający_przelew_wychodzący(self):
-        account = KontoFirmowe(self.nazwa_firmy,self.nip)
-        account.saldo = 800
+        account = CompanyAccount(self.company_name,self.nip)
+        account.balance = 800
         account.zaksięguj_przelew_wychodzący(700)
-        self.assertEqual(account.saldo, 800-700)
+        self.assertEqual(account.balance, 800-700)
 
     def test_działający_przelew_przychodzący(self):
-        account = KontoFirmowe(self.nazwa_firmy,self.nip)
-        account.saldo = 800
+        account = CompanyAccount(self.company_name,self.nip)
+        account.balance = 800
         account.zaksięguj_przelew_przychodzący(700)
-        self.assertEqual(account.saldo, 800+700)
+        self.assertEqual(account.balance, 800+700)
 
     def test_udany_wychodzący_przelew_ekspresow_firmowe(self):
-        account = KontoFirmowe(self.nazwa_firmy,self.nip)
-        account.saldo = 800
+        account = CompanyAccount(self.company_name,self.nip)
+        account.balance = 800
         account.zaksięguj_wychodzacy_przelew_ekspresowy(700)
-        self.assertEqual(account.saldo, 800-700-5)
+        self.assertEqual(account.balance, 800-700-5)
 
-    def test_udany_wychodzący_przelew_ekspresowy_konto_ujemne_o_opłatę(self):
-        account = KontoFirmowe(self.nazwa_firmy,self.nip)
-        account.saldo = 800
+    def test_udany_wychodzący_przelew_ekspresowy_account_ujemne_o_opłatę(self):
+        account = CompanyAccount(self.company_name,self.nip)
+        account.balance = 800
         account.zaksięguj_wychodzacy_przelew_ekspresowy(800)
-        self.assertEqual(account.saldo, 800-800-5)
+        self.assertEqual(account.balance, 800-800-5)
 
     def test_nieudany_wychodzący_przelew_ekspresowy(self):
-        account = KontoFirmowe(self.nazwa_firmy,self.nip)
-        account.saldo = 100
+        account = CompanyAccount(self.company_name,self.nip)
+        account.balance = 100
         account.zaksięguj_wychodzacy_przelew_ekspresowy(800)
-        self.assertEqual(account.saldo,100)
+        self.assertEqual(account.balance,100)
 
 
 
